7514cf485e00d35ece68fcb075bb656d35f79708
Abinav <abighimire@yahoo.com>
2024-07-11 19:47:01 +0545

    :sparkles: feat: initialized frontend and docker compose service added for next app

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+
+function Home() {
+  return (
+    <>
+      <Head>
+        <title> {"Top Page"}</title>
+      </Head>
+      <div> This is test page</div>
+    </>
+  );
+}
+
+export default Home;
